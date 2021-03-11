2b4ea936f57af904fcf5b8829d581d0e93686dee
dnavault <47720676+dnavault@users.noreply.github.com>
2021-03-10 23:14:27 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+    <DnaGrid/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import DnaGrid from "./components/DnaGrid";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    DnaGrid
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DnaGrid.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>DNAGrid-Main-Container</div>
+</template>
+
+<script>
+export default {
+  name: "DnaGrid",
+};
+</script>
